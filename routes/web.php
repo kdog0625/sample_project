420a7e55737c371fdb32b8ec389712241a65948d
kdog0625 <kdoooog34@gmail.com>
2020-07-31 22:32:02 +0900

    tweets設定

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,10 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('tweets/index');
 });
 
-Route::get('tweets', 'HelloController@index');
